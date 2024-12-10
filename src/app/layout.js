e03cb057e4e697160a293e88ac64a8b1476626a5
kysyl <kysylyov.artem@gmail.com>
2024-12-10 20:01:39 +0200

    global styles and variables, start building components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,17 @@
+// Imports 
 import localFont from "next/font/local"
 import "./globals.css"
 
+// Import components
+import Navbar from '@/components/navbar/Navbar' 
+import Footer from '@/components/footer/Footer' 
+
 const generalSans = localFont({
-  src: "./fonts/GeneralSans-Regular.woff",
+  src: "./fonts/GeneralSans-Regular.woff2",
   variable: "--font-general-sans",
   weight: "400",
+  subsets: ['latin'],
+  display: "swap",
 })
 
 export const metadata = {
@@ -15,8 +22,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${generalSans.variable}`}>
-        {children}
+      <body className={generalSans.className}>
+        <div className='wrapper'>
+          <Navbar/>
+          {children}
+          <Footer/>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+// Imports 
+import React from 'react'
+import styles from './styles.module.css'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
