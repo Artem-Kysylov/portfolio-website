4ff5c07f6ca0f59d66faecc0b1a16eb255048f3e
kysyl <kysylyov.artem@gmail.com>
2024-12-11 12:07:30 +0200

    start working with navbar

---
+++ src/app/data/navData.js
@@ -0,0 +1,27 @@
+// Navigation data 
+export const navItems = [
+    {
+        id: 1,
+        title: 'Services',
+        link: '#Services_section',
+    },
+    {
+        id: 2,
+        title: 'Projects',
+        link: '#Projects_section',
+    },
+    {
+        id: 3,
+        title: 'About',
+        link: '#About_section',
+    },
+]
+
+// Socials data 
+export const socialsItems = [
+    {
+        id: 1,
+        icon: '',
+        link: 'https://www.behance.net/kysylyovar7e7c',
+    },
+]
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 // Imports 
 import React from 'react'
+import Image from "next/image"
 import styles from './styles.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="container">
+        <div className={styles.nav__inner}>
+          <Image
+            src="/logo.svg"
+            width={199}
+            height={21}
+            alt="Logo"
+          />
+
+          <ul></ul>
+          
+          <ul></ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
