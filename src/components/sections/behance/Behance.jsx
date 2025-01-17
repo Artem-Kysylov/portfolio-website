60d2ea1a93e721fe00f92539d0556f02d12f2824
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-01-17 20:12:42 +0200

    add behance parallax animation

--- src/components/sections/behance/Behance.jsx
+++ src/components/sections/behance/Behance.jsx
@@ -1,26 +1,60 @@
+'use client'
 // Imports 
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import styles from './styles.module.css'
+import { useTransform, useScroll } from 'framer-motion'
+import Lenis from '@studio-freight/lenis'
 import { behanceImages } from '@/app/data/data'
 
 // Import components 
 import Button from '@/components/button/Button'
 import BehanceGalletyColumn from './BehanceGalletyColumn'
 
-// helpful link
-// https://blog.olivierlarose.com/tutorials/smooth-parallax-scroll
 
 const Behance = () => {
+  const gallery = useRef(null)
+  const [dimension, setDimension] = useState({ width: 0, height: 0 })
+
+  const { scrollYProgress } = useScroll({
+    target: gallery,
+    offset: ['start end', 'end start']
+  })
+
+  const { height } = dimension
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2])
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
+
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    const raf = (time) => {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    const resize = () => {
+      setDimension({width: window.innerWidth, height: window.innerHeight})
+    }
+
+    window.addEventListener("resize", resize)
+    requestAnimationFrame(raf);
+    resize();
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    }
+  }, [])
+
   return (
     <section className={styles.behance__section}>
       <h2 className={styles.behance__title}>behance works</h2>
-      <div className={styles.behance__gallery}>
-        <div className={styles.behance__galleryWrapper}>
-          <BehanceGalletyColumn behanceImages={[behanceImages[0], behanceImages[1], behanceImages[2]]}/>
-          <BehanceGalletyColumn behanceImages={[behanceImages[3], behanceImages[4], behanceImages[5]]}/>
-          <BehanceGalletyColumn behanceImages={[behanceImages[6], behanceImages[7], behanceImages[8]]}/>
-          <BehanceGalletyColumn behanceImages={[behanceImages[9], behanceImages[10], behanceImages[11]]}/>
-        </div>
+      <div className={styles.behance__gallery} ref={gallery}>
+          <BehanceGalletyColumn behanceImages={[behanceImages[0], behanceImages[1], behanceImages[2]]} y={y}/>
+          <BehanceGalletyColumn behanceImages={[behanceImages[3], behanceImages[4], behanceImages[5]]} y={y2}/>
+          <BehanceGalletyColumn behanceImages={[behanceImages[6], behanceImages[7], behanceImages[8]]} y={y3}/>
+          <BehanceGalletyColumn behanceImages={[behanceImages[9], behanceImages[10], behanceImages[11]]} y={y4}/>
       </div>
       <div className={styles.behance__btnWrapper}>
         <div className={styles.behnace__btnContainer}>
