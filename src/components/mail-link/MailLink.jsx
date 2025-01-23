d168faee141273b822d5dde737cd4490b69061a2
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-01-23 20:01:08 +0200

    add animation to mail link component

--- src/components/mail-link/MailLink.jsx
+++ src/components/mail-link/MailLink.jsx
@@ -6,10 +6,7 @@ import Link from 'next/link'
 const MailLink = () => {
   return (
     <Link href={'mailto: kysylyov.artem@gmail.com'}>
-      <div className={styles.mail__wrapper}>
         <p className={styles.mail__link}>kysylyov.artem@gmail.com</p>
-      </div>
-
     </Link>
   )
 }
