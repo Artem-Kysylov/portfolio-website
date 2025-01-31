2a1138ae6a6a0fd89b0f0f89b759db562a8bde5b
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-01-31 19:43:16 +0200

    finish work with form, testing emails

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,23 +1,32 @@
+'use client'
 // Imports 
-import React from 'react'
+import React, { useState } from 'react'
 import { IoCloseOutline } from "react-icons/io5"
 import { GoCheckCircleFill } from "react-icons/go"
 import styles from './styles.module.css'
 
 const Popup = () => {
+  const [popup, setPopup] = useState(true)
+
+  const handlePopup = () => {
+    setPopup(!popup)
+  }
+
   return (
-    <div className={styles.popup__wrapper}>
-        <div className={styles.popup__item}>
-            <button className={styles.popup__btn}>
-                <IoCloseOutline size={24}/>
-            </button>
-            <div className={styles.popup__content}>
-                <GoCheckCircleFill size={40}/>
-                <h4>thank you!</h4>
-                <p>I will contact to you soon as possible!</p>
-            </div>
-        </div>
-    </div>
+    popup && (
+      <div className={styles.popup__wrapper}>
+          <div className={styles.popup__item}>
+              <button className={styles.popup__btn} onClick={handlePopup}>
+                  <IoCloseOutline size={24}/>
+              </button>
+              <div className={styles.popup__content}>
+                  <GoCheckCircleFill size={40}/>
+                  <h4>thank you!</h4>
+                  <p>I will contact to you soon as possible!</p>
+              </div>
+          </div>
+      </div>
+    )
   )
 }
 
